512eef031d011a4978ee8c24643a67d5c97566a7
arditarizk <arditarizkif@gmail.com>
2026-03-31 00:51:59 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
-    // 1. URL Google Apps Script Anda 
-    // (Jika API berubah, cukup ganti link di bawah ini saja!)
+    // URL Google Apps Script Anda yang sudah sukses
     API_URL: "https://script.google.com/macros/s/AKfycbxHjPjVfzD2QYpxGgZLAC0jhBReN4nFFw-Yi1vfY4rG_BgF6zTxr0_J0IwFR7WDyH3T/exec",
     
-    // 2. Nomor WhatsApp Admin (Untuk menerima KTP & Jaminan)
+    // Nomor WA Admin (Penerima Form & Konfirmasi)
     WA_ADMIN: "62895410571547" 
 };
