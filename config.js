0cc9b6bf3f3bc093f99dcfdf2caa482eec9352c5
arditarizk <arditarizkif@gmail.com>
2026-04-02 22:42:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 const CONFIG = {
-    // Link API Google Script (Baru - Super Stabil V18)
+    // Link API Google Script (Versi Stabil V18)
     API_URL: "https://script.google.com/macros/s/AKfycbx66f-04RTIUmdEPX-yrVLWD_fC3yf4JAMP5EnOQEz_OCKvjoskjin2O3f7fdH4-F-duQ/exec",
     
     // Nomor WA Admin Zenith Cell
     WA_ADMIN: "62895410571547",
     
     // --- PENGATURAN KEAMANAN & MULTI-ADMIN ---
-    PIN_ADMIN: "zenith123", // Password master (Bisa untuk darurat)
-    
-    // Daftar ID Pengguna dan Sandi Kasir (Bisa ditambah/dihapus sesukamu)
+    PIN_ADMIN: "zenith123", 
     DAFTAR_ADMIN: {
         "ARDITA": { sandi: "123456", namaLengkap: "Ardita Rizki F." },
         "VIVI": { sandi: "654321", namaLengkap: "Vivi Nur D." },
-        "ADMIN": { sandi: "zenith123", namaLengkap: "Admin Pusat" } // Akun cadangan
+        "ADMIN": { sandi: "zenith123", namaLengkap: "Admin Pusat" } 
     },
     
     // --- SKEMA VOUCHER MARGIN ---
@@ -21,12 +19,15 @@ const CONFIG = {
     VOUCHERS: {
         "dulurdewe22": { margin: 22, pesan: "Kode valid! Margin layanan turun jadi 22% 🎉" },
         "nawakewed": { margin: 20, pesan: "Kode valid! Margin VVIP 20% 🎉" },
-        "birthday": { margin: 23, pesan: "Happy Birthday, Ardita! Margin super ringan 23% 🎉" }
+        
+        // 🚀 FITUR MEMBER GET MEMBER (REFERRAL)
+        "TEMANVIVI": { margin: 20, pesan: "Voucher Teman Vivi Aktif! Margin VVIP 20% + Vivi dapat Cashback! 🎁" },
+        "TEMANARDITA": { margin: 20, pesan: "Voucher Teman Ardita Aktif! Margin VVIP 20% + Cashback 50Rb! 🎁" }
     },
 
     // --- PENGATURAN PENGUMUMAN BERJALAN (MARQUEE) ---
-    PENGUMUMAN_AKTIF: true, // Ubah ke false jika ingin menyembunyikan pengumuman
+    PENGUMUMAN_AKTIF: true, 
     
-    // TEKS PROMO & PENGUMUMAN (Gen-Z Syariah Edition)
-    TEKS_PENGUMUMAN: "✨ Selamat Idul Fitri 1447 H! Promo VIP Margin 15% buat silaturahmi makin HD! 📱 HP baru Alhamdulillah, cicilan aman Astaghfirullah... tenang, di sini murni syariah tanpa denda! 💸 Skip riba, go syariah! Biar rezeki lancar dan FYP jalur langit. 🤲 Niatnya nabung buat nikah, malah checkout iPhone boba. Gak apa-apa, yang penting akadnya halal! 🚀 Minimal HP spek dewa, ibadah juga harus luar biasa. Yuk ajukan sekarang!"
+    // TEKS PROMO (Gen-Z Syariah Edition)
+    TEKS_PENGUMUMAN: "✨ Cari HP impian tapi gak ada di brosur? Pake JASTIP ZENITH aja, kami carikan & hitung cicilannya! 📱 Ajak teman kredit di Zenith pakai kodemu, dapatkan CASHBACK 50 Ribu tanpa diundi! 💸 Skip riba, go syariah! Biar rezeki lancar dan FYP jalur langit. 🤲"
 };
