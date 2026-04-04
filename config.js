9dcade744566f363116841813eb9b6d1a41ccc33
arditarizk <arditarizkif@gmail.com>
2026-04-04 21:29:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -38,5 +38,5 @@ const CONFIG = {
     "PENGUMUMAN_AKTIF": true,
     "TEKS_PENGUMUMAN": "✨ Cari HP impian tapi gak ada di brosur? Pake JASTIP ZENITH aja, kami carikan & hitung cicilannya! 📱 Ajak teman kredit di Zenith pakai kodemu, dapatkan CASHBACK 50 Ribu tanpa diundi! 💸 Skip riba, go syariah! Biar rezeki lancar dan FYP jalur langit. 🤲",
     "NAMA_TOKO": "Zenith Cell",
-    "LOGO_URL": ""
+    "LOGO_URL": "https://i.supaimg.com/74ab27fb-fcc0-4c79-86c8-843a9b5299c2/5daa1530-3351-436c-abc0-38c81c73669f.png"
 };
